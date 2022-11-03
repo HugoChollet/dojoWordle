adc3c454ba5803f78abfc8d01657387fcc9eaa5d
Hugo Chollet <hugochollet@Hugos-MBP.lan>
2022-11-03 17:46:33 +0100

    edit game component

---
+++ dojoWordle/components/Game/Game.style.tsx
@@ -0,0 +1,8 @@
+import styled from "@emotion/native";
+
+export const ResultContainer = styled.View({
+  alignItems: "center",
+  flexDirection: "row",
+  justifyContent: "center",
+  paddingBottom: 50,
+});
