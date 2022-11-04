76af5e44d21a0664b6c9bd673e8cf4dc82ffa19f
Hugo Chollet <hugochollet@Hugos-MacBook-Pro.local>
2022-11-04 14:57:07 +0100

    add spacer component for game

--- dojoWordle/components/Game/Game.style.tsx
+++ dojoWordle/components/Game/Game.style.tsx
@@ -1,8 +1,15 @@
 import styled from "@emotion/native";
 
+export const SpacerVertical = styled.View({
+  height: 50,
+});
+
+export const SpacerHonrizontal = styled.View({
+  width: 50,
+});
+
 export const ResultContainer = styled.View({
   alignItems: "center",
   flexDirection: "row",
   justifyContent: "center",
-  paddingBottom: 50,
 });
